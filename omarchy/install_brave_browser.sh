c9d1c7659cda41d476c0d23ccf49d85de1048c09
Moritz <119802108+4x4Robots@users.noreply.github.com>
2026-07-16 18:40:58 +0200

    install brave browser in omarchy

---
+++ omarchy/install_brave_browser.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Installing Brave (a privacy first browser)..."
+
+# Use AUR helper to install Brave
+yay -Sy brave-bin
+
+# Use official install script
+# curl -fsS https://dl.brave.com/install.sh | sh
+
